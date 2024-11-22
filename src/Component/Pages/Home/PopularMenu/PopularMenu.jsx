ea3a4e62fea8b25c1915b649696bd3a1105e0c5c
musharofahmed1 <musharofahmed11@gmail.com>
2024-11-21 19:55:35 -0600

    Working On Popular Menu

--- src/Component/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Component/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect( () =>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            
+            const popularItems = data.filter(item => item.category === 'popular') ;
+            setMenu (popularItems)})
+    }, [])
     return (
         <section>
             <SectionTitle
@@ -10,6 +21,16 @@ const PopularMenu = () => {
                 subHeading = "Popular Items"
             
             ></SectionTitle>
+
+            <div>
+                {
+                    menu.map(item => <MenuItem
+                    key = {item._id}
+                    item={item}
+                    >
+                    </MenuItem>)
+                }
+            </div>
         </section>
     );
 };
